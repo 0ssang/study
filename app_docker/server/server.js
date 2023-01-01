1d4e9a305c1bd5479d46e9446342a3f1f2466c50
조영상 <joyoungsang@joyeongsang-ui-MacBookAir.local>
2023-01-01 23:47:25 +0900

    update

--- app_docker/server/server.js
+++ app_docker/server/server.js
@@ -1,16 +1,28 @@
 'use strict';
 
 const express = require('express');
-
 const PORT = 8080;
 const HOST = '0.0.0.0';
-
-
 const app = express();
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST, () => {
+app.get('/api/list', (req, res) => {
+  res.json([
+    {id: 'test1', name: '테스트1'},
+    {id: 'test2', name: '테스트2'},
+    {id: 'test3', name: '테스트3'},
+    {id: 'test4', name: '테스트4'},
+  ]);
+});
+
+app.listen(PORT, HOST, (err) => {
+  if(err){
+    console.log('err 발생');
+  }
   console.log(`Running on http://${HOST}:${PORT}`);
 });
